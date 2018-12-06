3594f8ebfdb8b0f8369824c32db79c4c13ad2dbe
Nicolas <37118153+Nickeroro@users.noreply.github.com>
2018-12-06 10:10:39 +0100

    Add files via upload

---
+++ MD5_Wordlist.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo  "MD5_wordlist will try to find the salted MD5 hashed messages you've given <3"
+echo  "MD5 hashed message should be as it follows: /1 /SALT /THE_SALTED_MD5"
+echo  "Input the salt:"
+read salt
+echo "Input the MD5 hashed message:"
+read hmd5
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    echo "Text read from file: $line"
+	OUTPUT="$(openssl $line -1 -salt salt)"
+	echo "$OUTPUT"
+	
+	'if [OUTPUT = hmd5]; then
+		echo "$line"
+	fi'
+	
+done < "$1"
